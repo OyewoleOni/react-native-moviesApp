96dbe7178f3e6df766cc9eaf00ce751f57dcab87
Oyewole Oni <ooni@eminenttechnology.com>
2022-03-03 01:25:31 +0100

    Adding Image Slider, Styling Immage Slider and Adding a flatlist component

--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,66 @@
 import React, { useEffect, useState} from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, FlatList } from 'react-native'
+import { SliderBox } from "react-native-image-slider-box";
+import { getPopularMovies, getUpcomingMovies } from '../services/services';
 
-import { getPopularMovies } from '../services/services';
+const dimensions = Dimensions.get('screen');
 const Home = () => {
     let title = 'movies name'
-    const [movie, setMovie] = useState('');
+    const [moviesImages, setMoviesImages] = useState('');
+    const [popularMovies, setPopularMovies] = useState('');
     const [error, setError] = useState(false);
     useEffect(() => {
+        getUpcomingMovies().then(movies => {
+            const moviesImagesArray = [];
+            movies.forEach( movie => {
+                moviesImagesArray.push('https://image.tmdb.org/t/p/w500'+movie.poster_path);
+            });
+            setMoviesImages(moviesImagesArray)
+        }).catch(err =>{
+            setError(err);
+        })
       getPopularMovies().then(movies => {
-        setMovie(movies[0])
+          setPopularMovies(movies)
       }).catch(err =>{
         setError(err);
       });
     }, [])
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Movie Name: {movie.original_title}</Text>
-            <Text> Movie lang: {movie.original_language}</Text>
-            <Text> Release Date: {movie.release_date}</Text> 
-        {error && <Text style={{ color: 'red'}}>Server Error</Text> } 
-    </View>
+      <>
+            <View style={styles.sliderContainer}>
+            <SliderBox 
+                        images={moviesImages} 
+                        dotStyle={styles.sliderDotStyle}
+                        sliderBoxHeight={dimensions.height /1.5}
+                        autoplay={true}  
+                        circleLoop={true}
+                        />
+            </View>
+            <View style={styles.carousel}>
+                <FlatList data={popularMovies} 
+                         horizontal = {true}
+                         renderItem={({item}) => <Text>{item.title}</Text>}/>
+            </View>
+      </>
+    
   )
 }
+
+const styles = StyleSheet.create({
+    sliderContainer: {
+        flex: 1, 
+        justifyContent: "center", 
+        alignItems: "center",
+    },
+    sliderDotStyle : {
+        height: 0
+    },
+    carousel: {
+        flex: 1, 
+        justifyContent: "center", 
+        alignItems: "center",
+    }
+})
 
 export default Home
